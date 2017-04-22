6b50eb1c9ad9ffef2236261a7fc3556baf03229a
shenhaiyudreamlife <1169417283@qq.com>
2017-04-22 15:16:18 +0800

    header and footer

--- src/scripts/components/common/Footer.js
+++ src/scripts/components/common/Footer.js
@@ -3,9 +3,28 @@ import React,{Component} from 'react'
 class Footer extends Component{
   render(){
     return (
-      <div>
-      Footer
-      </div>
+      <footer>
+        <div className="home">
+          <i></i>
+          <span>首页</span>
+        </div>
+        <div className="classify">
+          <i></i>
+          <span>分类</span>
+        </div>
+        <div className="shopping_car_wrap">
+          <div className="shopping_car">
+          </div>
+        </div>
+        <div className="order">
+          <i></i>
+          <span>订单</span>
+        </div>
+        <div className="my">
+          <i></i>
+          <span>我的</span>
+        </div>
+      </footer>
     )
   }
 }
