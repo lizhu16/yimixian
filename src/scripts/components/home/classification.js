86f8ea9c271805e9c57e37eb483073c3583a92e9
shenhaiyudreamlife <1169417283@qq.com>
2017-04-23 11:08:29 +0800

    04-23 11:08 update

---
+++ src/scripts/components/home/classification.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react'
+
+
+class Classification extends Component{
+  render(){
+    return (
+      <div className="liangfan">
+        <div className="title">
+          <i>量贩装</i>
+          <b>更多商品></b>
+        </div>
+        <div>
+          <a href="#javascript:void(0);">
+            <img src="http://7sbnc0.com2.z0.glb.qiniucdn.com/material/2016/11/21/hdsyliangfanzhuang_48108.png?imageView2/2/w/640"/>
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Classification
