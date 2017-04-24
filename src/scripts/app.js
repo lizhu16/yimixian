cf0482d5690ac43c9b4ca4edc28ec7c42f39b289
nuan1993 <xiaonieming@163.com>
2017-04-24 11:59:40 +0800

    run classify

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom'
 import {Router,Route,hashHistory,IndexRedirect} from 'react-router'
 import Index from './components'
 import Home from './components/home/home'
+import GoodList from './components/list/GoodList'
 
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={Index}>
       <IndexRedirect to="/home"/>
       <Router path="home" component={Home}/>
+      <Router path="classify" component={GoodList}/>
     </Route>
   </Router>
   ),
