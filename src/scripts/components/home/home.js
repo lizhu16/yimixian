7a86139f4032c26b2695aab5d7db86dd22b55b15
shenhaiyudreamlife <1169417283@qq.com>
2017-04-25 08:56:14 +0800

    add scroller

--- src/scripts/components/home/home.js
+++ src/scripts/components/home/home.js
@@ -1,8 +1,9 @@
 import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
 import HomeList from './homeList'
 import Classification from './classification'
 import Nav from './nav'
-
+import Scroller from '../../../component_dev/scroller/src'
 class Home extends Component{
   constructor(props){
     super(props)
@@ -14,19 +15,29 @@ class Home extends Component{
 
   render(){
     return (
-      <section>
-        <div className="top">
-          <img src="http://7sbnc0.com2.z0.glb.qiniucdn.com/material/2017/4/19/yewushengji_19852.jpeg?imageView2/2/w/640"/>
-        </div>
-        <Nav home_data={this.state.data.card_list} />
-        <HomeList home_data={this.state.data.card_list} />
-        <Classification home_data={this.state.data.card_list} />
-        <div  className="goods_more">
-          <a href="#javascript:void(0)">
-            <span><b></b>点击查看全部商品<i className="yo-ico">&#xf07f;</i></span>
-          </a>
-        </div>
-      </section>
+      <Scroller extraClass={'yo-scroller-a'}
+        ref="scroller"
+        usePullRefresh={true}
+        onRefresh={() => {
+        setTimeout(()=>{
+            this.refs.scroller.stopRefreshing(true); // 这个调用也可以放在异步操作的回调里之后
+        },2000)
+    }}
+      >
+        <section>
+          <div className="top">
+            <img src="http://7sbnc0.com2.z0.glb.qiniucdn.com/material/2017/4/19/yewushengji_19852.jpeg?imageView2/2/w/640"/>
+          </div>
+          <Nav home_data={this.state.data.card_list} />
+          <HomeList home_data={this.state.data.card_list} />
+          <Classification home_data={this.state.data.card_list} />
+          <div  className="goods_more">
+            <a href="#javascript:void(0)">
+              <span><b></b>点击查看全部商品<i className="yo-ico">&#xf07f;</i></span>
+            </a>
+          </div>
+        </section>
+    </Scroller>
     )
   }
 
